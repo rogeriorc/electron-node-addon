a639416c3d245e0470a7bc31d6cc0f5b91e90c80
Rogério Ribeiro da Cruz <rogeriorc@gmail.com>
2019-03-19 16:05:11 -0300

    .

--- src/node-addon/context_aware/main.cc
+++ src/node-addon/context_aware/main.cc
@@ -3,9 +3,8 @@
 using namespace v8;
 
 class AddonData {
- public:
-  AddonData(Isolate* isolate, Local<Object> exports):
-      call_count(0) {
+public:
+  AddonData(Isolate *isolate, Local<Object> exports) : call_count(0) {
     // Link the existence of this object instance to the existence of exports.
     exports_.Reset(isolate, exports);
     exports_.SetWeak(this, DeleteMe, WeakCallbackType::kParameter);
@@ -22,48 +21,61 @@ class AddonData {
   // Per-addon data.
   int call_count;
 
- private:
+private:
   // Method to call when "exports" is about to be garbage-collected.
-  static void DeleteMe(const WeakCallbackInfo<AddonData>& info) {
+  static void DeleteMe(const WeakCallbackInfo<AddonData> &info) {
     delete info.GetParameter();
   }
 
   // Weak handle to the "exports" object. An instance of this class will be
   // destroyed along with the exports object to which it is weakly bound.
-  v8::Persistent<v8::Object> exports_;
+  Persistent<Object> exports_;
 };
 
-static void Method(const v8::FunctionCallbackInfo<v8::Value>& info) {
+static void Method(const FunctionCallbackInfo<Value> &info) {
   // Retrieve the per-addon-instance data.
-  AddonData* data =
-      reinterpret_cast<AddonData*>(info.Data().As<External>()->Value());
+  AddonData *data =
+      reinterpret_cast<AddonData *>(info.Data().As<External>()->Value());
   data->call_count++;
   info.GetReturnValue().Set((double)data->call_count);
 }
 
-#if NODE_VERSION_AT_LEAST(10, 0, 0)
+#if NODE_VERSION_AT_LEAST(10, 3, 0)
+
 // Initialize this addon to be context-aware.
 NODE_MODULE_INIT(/* exports, module, context */) {
+
 #else
-void Initialize(v8::Local<v8::Object> exports, v8::Local<v8::Value> module, v8::Local<v8::Context> context, void* priv) {
+
+void Initialize(Local<Object> exports, 
+				Local<Value> module,
+                Local<Context> context, 
+				void *priv);
+
+NODE_MODULE_CONTEXT_AWARE(context_aware, Initialize)
+
+void Initialize(Local<Object> exports, 
+				Local<Value> module,
+                Local<Context> context, 
+				void *priv) {
+
 #endif
-	Isolate* isolate = context->GetIsolate();
+  Isolate *isolate = context->GetIsolate();
 
   // Create a new instance of AddonData for this instance of the addon.
-  AddonData* data = new AddonData(isolate, exports);
+  AddonData *data = new AddonData(isolate, exports);
   // Wrap the data in a v8::External so we can pass it to the method we expose.
   Local<External> external = External::New(isolate, data);
 
   // Expose the method "Method" to JavaScript, and make sure it receives the
   // per-addon-instance data we created above by passing `external` as the
   // third parameter to the FunctionTemplate constructor.
-  exports->Set(context,
-               String::NewFromUtf8(isolate, "method", NewStringType::kNormal)
-                  .ToLocalChecked(),
-               FunctionTemplate::New(isolate, Method, external)
-                  ->GetFunction(context).ToLocalChecked()).FromJust();
+  exports
+      ->Set(context,
+            String::NewFromUtf8(isolate, "method", NewStringType::kNormal)
+                .ToLocalChecked(),
+            FunctionTemplate::New(isolate, Method, external)
+                ->GetFunction(context)
+                .ToLocalChecked())
+      .FromJust();
 }
-
-#if !NODE_VERSION_AT_LEAST(10, 0, 0)
-NODE_MODULE_CONTEXT_AWARE(context_aware, Initialize)
-#endif
